0db2f34f65a79660fcac123e0e8a70e918d662bc
jarora jarora@IN-jarora-W2.adprod.bmc.com <jalajkumar.arora@bmc.com>
2023-10-25 14:58:19 +0530

    update

--- index.js
+++ index.js
@@ -12,46 +12,20 @@ const axios = require('axios').default;
 
 try 
 {
-  let buildParms;
   let inputs = ['ces_url', 'ces_token', 'srid', 'todaysDate', 'priorWeek', 'startDate', 'endDate',
   'requestId', 'setId', 'environment', 'status'];
   inputs = utils.retrieveInputs(core, inputs);
   core.info('Code Pipeline: parsed inputs: ' + utils.convertObjectToJson(inputs));
 
-  /*
-  if (utils.stringHasContent(inputs.generate_automatically)) {
-    console.log('Generate parameters are being retrieved from the ' +
-      'generate_automatically input.');
-    buildParms = utils.parseStringAsJson(inputs.generate_automatically);
-  } else {
-    console.log('Generate parameters are being retrieved from the inputs.');
-    buildParms = getParmsFromInputs(inputs.assignment_id, inputs.level, inputs.task_id);
-  }
-  core.info('Code Pipeline: parsed buildParms: ' + utils.convertObjectToJson(buildParms));
-
-  //const requiredFields = ['containerId', 'taskLevel', 'taskIds'];
-  if (!utils.validateBuildParms(buildParms, requiredFields)) {
-    throw new MissingArgumentException(
-        'Inputs required for Code Pipeline Generate are missing. ' +
-      '\nSkipping the generate request....');
-  }*/
-
-  //const reqPath = getGenerateAwaitUrlPath(inputs.srid, buildParms);
-  //const reqUrl = utils.assembleRequestUrl(inputs.ces_url, reqPath);
-  let reqPath = `/ispw/${inputs.srid}/deployments`;
-  const reqUrl = prepareRequestUrl(inputs.ces_url, reqPath);
-  core.info('Code Pipeline: request url: ' + reqUrl.href); 
-  core.info('Code Pipeline: request url: ' + reqUrl); 
-  const reqBodyObj = prepareRequestBodyObject(inputs.todaysDate, inputs.priorWeek, inputs.startDate,
-     inputs.endDate, inputs.requestId,inputs.setId, inputs.environment, inputs.status);
-  core.info('Code Pipeline: reqBodyObj : ' + reqBodyObj); 
-  core.info('Code Pipeline: reqBodyObj : ' + reqBodyObj); 
-  core.info('Code Pipeline: token : ' + inputs.ces_token);
-  getHttpGetPromise(reqUrl, inputs.ces_token, reqBodyObj)
+  let requestBasePath = `/ispw/${inputs.srid}/deployments`;
+  let requestQueryPath = prepareRequestQueryPath(inputs);
+  const requestUrl = prepareRequestUrl(inputs.ces_url, requestBasePath, requestQueryPath);
+  core.info('Code Pipeline: request url: ' + requestUrl.href); 
+ 
+  getHttpGetPromise(requestUrl, inputs.ces_token)
       .then((response) => {
         core.info('Code Pipeline: received response body: ' +
          utils.convertObjectToJson(response.data));
-        // generate could have passed or failed
         setOutputs(core, response.data);
         return handleResponseBody(response.data);
       },
@@ -62,7 +36,7 @@ try
           core.info('Code Pipeline: received error response body: ' +
             utils.convertObjectToJson(error.response.data));
           setOutputs(core, error.response.data);
-          throw new GenerateFailureException(error.response.data.message);
+          throw new DeploymentListFailureException(error.response.data.message);
         }
         throw error;
       })
@@ -74,43 +48,65 @@ try
 
   // the following code will execute after the HTTP request was started,
   // but before it receives a response.
-  //console.log('Starting the generate process for task ' + buildParms.taskIds.toString());
-} catch (error) {
-  if (error instanceof MissingArgumentException) {
-    // this would occur if there was nothing to load during the sync process
-    // no need to fail the action if the generate is never attempted
-    console.log(error.message);
-  } else {
+} 
+catch (error) 
+{
     core.info(error.stack);
-    console.error('An error occurred while starting the generate');
+    console.error('An error occurred while calling the deployment list');
     core.setFailed(error.message);
-  }
 }
 
 /**
- * Gets a promise for sending an http POST request
+ * Gets a promise for sending an http GET request
  * @param {URL} requestUrl the URL to send hte request to
  * @param {string} token the token to use during authentication
- * @param {*} requestBody the request body object
  * @return {Promise} the Promise for the request
  */
-function getHttpGetPromise(requestUrl, token, requestBody) {
-  core.info('Jalaj requestUrl.href: ' + requestUrl.href );
-  core.info('Jalaj token: ' + token);
+function getHttpGetPromise(requestUrl, token) {
   const options = {
     headers: {
       'Content-Type': 'application/json',
       'authorization': token,
     },
   };
-  core.info('Jalaj requestUrl.href: ' + requestUrl.href );
-  core.info('Jalaj options: ' + options);
-  core.info('Jalaj options 0: ' + options.headers[0] );
-  core.info('Jalaj options 1: ' + options.headers[1] );
   return axios.get(requestUrl.href, options);
 }
 
-function prepareRequestUrl(cesUrl, requestPath) 
+function prepareRequestQueryPath(inputs)
+{
+  let requestQueryPath ="?";
+  if (utils.stringHasContent(inputs.startDate)) 
+  {
+    requestQueryPath.concat(`startDate=${inputs.startDate}&`);
+  }
+  if (utils.stringHasContent(inputs.endDate)) 
+  {
+    requestQueryPath.concat(`endDate=${inputs.endDate}&`);
+  }
+  if (utils.stringHasContent(inputs.requestId)) 
+  {
+    requestQueryPath.concat(`requestId=${inputs.requestId}&`);
+  }
+  if (utils.stringHasContent(inputs.setId)) 
+  {
+    requestQueryPath.concat(`setId=${inputs.setId}&`);
+  }
+  if (utils.stringHasContent(inputs.environment)) 
+  {
+    requestQueryPath.concat(`environment=${inputs.environment}&`);
+  }
+  if (utils.stringHasContent(inputs.status)) 
+  {
+    requestQueryPath.concat(`status=${inputs.status}&`);
+  }
+  if(requestQueryPath.endsWith('&') || requestQueryPath.endsWith('?') || requestQueryPath.endsWith('?&') )
+  {
+    requestQueryPath = requestQueryPath.substring(0, cesUrl.length - 1);
+  }
+  core.info('Code Pipeline: Request Query Url PAth: ' + requestQueryPath);
+}
+
+function prepareRequestUrl(cesUrl, requestBasePath, requestQueryPath) 
 {
   let lowercaseUrl = cesUrl.toLowerCase();
   
@@ -134,7 +130,7 @@ function prepareRequestUrl(cesUrl, requestPath)
     cesUrl = cesUrl.substr(0, cesUrl.length - 1);
   }
 
-  const tempUrlStr = cesUrl.concat(requestPath);
+  const tempUrlStr = cesUrl.concat(requestBasePath, requestQueryPath);
   const url = new URL(tempUrlStr);
   return url;
 }
@@ -142,32 +138,6 @@ function prepareRequestUrl(cesUrl, requestPath)
 /** *****************************************************************************************/
 // END OF MAIN SCRIPT, START OF FUNCTION DEFINITIONS
 /** *****************************************************************************************/
-
-/**
- * Uses the input parameters from the action metadata to fill in a BuildParms
- * object.
- * @param  {string} inputAssignment the assignmentId passed into the action
- * @param  {string} inputLevel the Code Pipeline level passed into the action
- * @param  {string} inputTaskId the comma separated list of task IDs passed
- * into the action
- * @return {BuildParms} a BuildParms object with the fields filled in.
- * This will never return undefined.
- */
-function getParmsFromInputs(inputAssignment, inputLevel, inputTaskId) {
-  const buildParms = {};
-  if (utils.stringHasContent(inputAssignment)) {
-    buildParms.containerId = inputAssignment;
-  }
-
-  if (utils.stringHasContent(inputLevel)) {
-    buildParms.taskLevel = inputLevel;
-  }
-
-  if (utils.stringHasContent(inputTaskId)) {
-    buildParms.taskIds = inputTaskId.split(',');
-  }
-  return buildParms;
-}
 
 /**
  * Takes the fields from the response body and sends them to the outputs of
@@ -178,86 +148,11 @@ function getParmsFromInputs(inputAssignment, inputLevel, inputTaskId) {
 function setOutputs(core, responseBody) {
   if (responseBody) {
     core.setOutput('output_json', utils.convertObjectToJson(responseBody));
-    core.setOutput('set_id', responseBody.setId);
-    core.setOutput('url', responseBody.url);
 
     const isTimedOut = utils.stringHasContent(responseBody.message) &&
       responseBody.message.includes('timed out');
     core.setOutput('is_timed_out', isTimedOut);
-
-    if (responseBody.awaitStatus) {
-      core.setOutput('generate_failed_count', responseBody.awaitStatus.generateFailedCount);
-      core.setOutput('generate_success_count', responseBody.awaitStatus.generateSuccessCount);
-      core.setOutput('has_failures', responseBody.awaitStatus.hasFailures);
-      core.setOutput('task_count', responseBody.awaitStatus.taskCount);
-    }
   }
-}
-
-/**
- * Gets the request path for the CES REST api generate-await on tasks. The returned path starts with
- * '/ispw/' and ends with the query parameters
- * @param {string} srid The SRID for this instance of Code Pipeline
- * @param {*} buildParms The build parms to use when filling out the request url
- * @return {string} the request path which can be appended to the CES url
- */
-function getGenerateAwaitUrlPath(srid, buildParms) {
-  let tempUrlStr = `/ispw/${srid}/assignments/${buildParms.containerId}`;
-  tempUrlStr = tempUrlStr.concat('/taskIds/generate-await?');
-  buildParms.taskIds.forEach((id) => {
-    tempUrlStr = tempUrlStr.concat(`taskId=${id}&`);
-  });
-  tempUrlStr = tempUrlStr.concat(`level=${buildParms.taskLevel}`);
-  return tempUrlStr;
-}
-
-/**
- * Prepares an object for the CES request body.
- * @param  {string | undefined} runtimeConfig the runtime configuration passed
- * in the inputs
- * @param  {string | undefined} changeType the change type passed in the inputs
- * @param  {string | undefined} executionStatus the execution status passed
- * in the inputs
- * @param  {string | undefined} autoDeploy whether to auto deploy
- * @return {any} an object with all the fields for the request body filled in
- */
-function prepareRequestBodyObject(todaysDate, priorWeek, startDate,
-  endDate, requestId, setId, environment, status) 
-  {
-    const requestBody = {};
-    if (utils.stringHasContent(todaysDate)) 
-    {
-      requestBody.todaysDate = todaysDate;
-    }
-    if (utils.stringHasContent(priorWeek)) 
-    {
-      requestBody.priorWeek = priorWeek;
-    }
-    if (utils.stringHasContent(startDate)) 
-    {
-      requestBody.startDate = startDate;
-    }
-    if (utils.stringHasContent(endDate)) 
-    {
-      requestBody.endDate = endDate;
-    }
-    if (utils.stringHasContent(requestId)) 
-    {
-      requestBody.requestId = requestId;
-    }
-    if (utils.stringHasContent(setId)) 
-    {
-      requestBody.setId = setId;
-    }
-    if (utils.stringHasContent(environment)) 
-    {
-      requestBody.environment = environment;
-    }
-    if (utils.stringHasContent(status))
-    {
-      requestBody.status = status;
-    }
-  return requestBody;
 }
 
 /**
@@ -271,19 +166,11 @@ function prepareRequestBodyObject(todaysDate, priorWeek, startDate,
 function handleResponseBody(responseBody) {
   if (responseBody === undefined) {
     // empty response
-    throw new GenerateFailureException(
-        'No response was received from the generate request.');
-  } else if (responseBody.awaitStatus === undefined) {
-    // Generate did not complete - there should be a message returned
-    if (responseBody.message !== undefined) {
-      throw new GenerateFailureException(responseBody.message);
-    }
-    throw new GenerateFailureException('The generate request did not complete successfully.');
-  } else if (responseBody.awaitStatus.generateFailedCount !== 0) {
-    // there were generate failures
-    console.error(utils.getStatusMessageToPrint(responseBody.awaitStatus.statusMsg));
-    throw new GenerateFailureException('There were generate failures.');
-  } else {
+    throw new DeploymentListFailureException(
+        'No response was received from the deployment list request.');
+  } 
+  else 
+  {
     // success
     console.log(utils.getStatusMessageToPrint(responseBody.awaitStatus.statusMsg));
     return responseBody;
@@ -291,33 +178,19 @@ function handleResponseBody(responseBody) {
 }
 
 /**
- * Error to throw when not all the arguments have been specified for the action.
- * @param  {string} message the message associated with the error
- */
-function MissingArgumentException(message) {
-  this.message = message;
-  this.name = 'MissingArgumentException';
-}
-MissingArgumentException.prototype = Object.create(Error.prototype);
-
-/**
  * Error to throw when the response for the generate request is incomplete
  *  or indicates errors.
  * @param  {string} message the message associated with the error
  */
-function GenerateFailureException(message) {
+function DeploymentListFailureException(message) {
   this.message = message;
-  this.name = 'GenerateFailureException';
+  this.name = 'DeploymentListFailureException';
 }
-GenerateFailureException.prototype = Object.create(Error.prototype);
+DeploymentListFailureException.prototype = Object.create(Error.prototype);
 
 
 module.exports = {
-  getParmsFromInputs,
   setOutputs,
-  getGenerateAwaitUrlPath,
-  prepareRequestBodyObject,
   handleResponseBody,
-  MissingArgumentException,
-  GenerateFailureException,
+  DeploymentListFailureException,
 };
